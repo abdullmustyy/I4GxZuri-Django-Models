079d6dc5803fb27ac74707632be55217df137175
abdullmustyy <abdulmusty03@outlook.com>
2022-06-18 21:08:37 +0100

    Create Django project and Blog app

---
+++ I4G024708PSU/blog/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from operator import mod
+from turtle import title
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+    created_date = models.DateTimeField(default=datetime.today)
+    published_date = models.DateTimeField(default=datetime.today)
